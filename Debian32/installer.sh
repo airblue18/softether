be5f523b69abd88f29087be4e9454406049e78ca
airblue <akosikolokoy18@gmail.com>
2018-07-12 11:26:51 +0800

    Update installer.sh

--- Debian32/installer.sh
+++ Debian32/installer.sh
@@ -2,8 +2,8 @@
 #Softether auto install script
 #Debian and Ubuntu
 #Created July 12, 2018
-#Latest Softether Server Version for May 2018 v4.27-9668-beta-2018.05.29-tree
-#Coded by airblue
+#Latest Softether Server Version for May 2018 v4.27-9668-beta-2018.05.26-tree
+#Coded by AIRBLUE
 #Open Source Project From https://www.softether.org/
 #From university of Tsukuba, Japan
 #--------------------------------------------------
@@ -15,8 +15,8 @@ cd /root
 apt-get update -y
 apt-get install build-essential -y
 #Downloading server files
-wget http://www.softether-download.com/files/softether/v4.27-9668-beta-2018.05.29-tree/Linux/SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x86-32bit.tar.gz
-tar zxf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x86-32bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.27-9668-beta-2018.05.29-tree/Linux/SoftEther_VPN_Server/32bit_-_ARM_EABI/softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-arm_eabi-32bit.tar.gz
+tar zxf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-arm_eabi-32bit.tar.gz
 cd vpnserver
 #Installing server files, Manual input
 clear
@@ -24,13 +24,13 @@ echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO 
 make
 cd /root
 mv vpnserver /usr/local
-rm -rf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x86-32bit.tar.gz
+rm -rf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-arm_eabi-32bit.tar.gz
 cd /usr/local/vpnserver
 chmod 600 *
 chmod 700 vpncmd
 chmod 700 vpnserver
 #Installing server command
-wget https://raw.githubusercontent.com/airblue18/master/Debian32/vpn-server.sh --no-check-certificate
+wget https://raw.githubusercontent.com/airblue18/softether/master/Debin32/vpn-server.sh --no-check-certificate
 mv vpn-server.sh /etc/init.d/vpnserver
 cd /etc/init.d/
 chmod 755 vpnserver
@@ -39,11 +39,12 @@ update-rc.d vpnserver defaults
 cd /usr/local/vpnserver
 echo ---------------------------------------------
 echo  -e "\033[32;5mVPN SERVER INSTALLED SUCCESSFULLY!\033[0m"
+echo "SoftEther auto installer by AIRBLUE"
 echo "vpncmd is at /usr/local/vpnserver"
 echo ---------------Commands----------------------
 echo /etc/init.d/vpnserver start - to start
 echo /etc/init.d/vpnserver restart - to restart
 echo /etc/init.d/vpnserver stop - to stop
 echo ---------------------------------------------
-echo ------------- Created By airblue -------------
+echo ------------- Created By AIRBLUE -------------
 #End of script
