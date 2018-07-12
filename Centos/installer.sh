971fd5b2453df98f1a0486f1b01aa8e74e1bfcc9
airblue <akosikolokoy18@gmail.com>
2018-07-12 13:49:40 +0800

    Update installer.sh

--- Centos/installer.sh
+++ Centos/installer.sh
@@ -1,9 +1,9 @@
 #--------------------------------------------------
 #Softether auto install script
 #Centos 7
-#Created May 25, 2018
-#Latest Softether Server Version for May 2018 v4.27-9667-beta-2018.05.26-tree
-#Coded by Jayson Velagio
+#Created July 12, 2018
+#Latest Softether Server Version for May 2018 v4.27-9668-beta-2018.05.29-tree
+#Coded by AIRBLUE
 #Open Source Project From https://www.softether.org/
 #From university of Tsukuba, Japan
 #--------------------------------------------------
@@ -16,8 +16,8 @@ yum update -y
 yum groupinstall "development tools" -y
 #Downloading server files
 yum install wget -y
-wget http://www.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
-tar zxf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.27-9668-beta-2018.05.29-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
+tar zxf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
 cd vpnserver
 clear
 echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO PROCEED\033[0m"
@@ -25,13 +25,13 @@ echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO 
 make
 cd /root
 mv vpnserver /usr/local
-rm -rf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+rm -rf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
 cd /usr/local/vpnserver
 chmod 600 *
 chmod 700 vpncmd
 chmod 700 vpnserver
 #Installing server command
-wget https://raw.githubusercontent.com/jaysonvelagio/SoftetherAutoInstallMultiPlatform/master/Centos/vpn-server.sh --no-check-certificate
+wget https://raw.githubusercontent.com/airblue18/softether/master/Centos/vpn-server.sh --no-check-certificate
 mv vpn-server.sh /etc/init.d/vpnserver
 cd /etc/init.d/
 chmod 755 vpnserver
@@ -40,10 +40,12 @@ chkconfig --add vpnserver
 cd /usr/local/vpnserver
 echo ---------------------------------------------
 echo  -e "\033[32;5mVPN SERVER INSTALLED SUCCESSFULLY!\033[0m"
+echo "SoftEther auto installer by AIRBLUE"
 echo "vpncmd is at /usr/local/vpnserver"
 echo ---------------Commands----------------------
 echo /etc/init.d/vpnserver start - to start
 echo /etc/init.d/vpnserver restart - to restart
 echo /etc/init.d/vpnserver stop - to stop
 echo ---------------------------------------------
+echo ------------- Created By airblue -------------
 #End of script
