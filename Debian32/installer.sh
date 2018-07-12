90be7f9cf93a6a93479ee3fe40c4355ab2fd389c
airblue <akosikolokoy18@gmail.com>
2018-07-12 10:18:14 +0800

    Add files via upload

---
+++ Debian32/installer.sh
@@ -0,0 +1,48 @@
+#--------------------------------------------------
+#Softether auto install script
+#Debian and Ubuntu
+#Created July 12, 2018
+#Latest Softether Server Version for May 2018 v4.27-9667-beta-2018.05.26-tree
+#Coded by airblue
+#Open Source Project From https://www.softether.org/
+#From university of Tsukuba, Japan
+#--------------------------------------------------
+#Login with root permission or execute #sudo su
+#Script start
+#--------------------------------------------------
+#Updating centos version
+cd /root
+apt-get update -y
+apt-get install build-essential -y
+#Downloading server files
+wget https://github.com/airblue18/softether/softether-vpnserver-beta-2018.05.29-linux-x86-32bit.tar.gz_or_softether-vpnserver-beta-2018.05.29-linux-arm_eabi-32bit.tar.gz
+cd vpnserver
+#Installing server files, Manual input
+clear
+echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO PROCEED\033[0m"
+make
+cd /root
+mv vpnserver /usr/local
+rm -rf softether-vpnserver-beta-2018.05.29-linux-x86-32bit.tar.gz
+cd /usr/local/vpnserver
+chmod 600 *
+chmod 700 vpncmd
+chmod 700 vpnserver
+#Installing server command
+wget https://raw.githubusercontent.com/airblue18/master/Debian32/vpn-server.sh --no-check-certificate
+mv vpn-server.sh /etc/init.d/vpnserver
+cd /etc/init.d/
+chmod 755 vpnserver
+update-rc.d vpnserver defaults
+/etc/init.d/vpnserver start
+cd /usr/local/vpnserver
+echo ---------------------------------------------
+echo  -e "\033[32;5mVPN SERVER INSTALLED SUCCESSFULLY!\033[0m"
+echo "vpncmd is at /usr/local/vpnserver"
+echo ---------------Commands----------------------
+echo /etc/init.d/vpnserver start - to start
+echo /etc/init.d/vpnserver restart - to restart
+echo /etc/init.d/vpnserver stop - to stop
+echo ---------------------------------------------
+echo ------------- Created By airblue -------------
+#End of script
